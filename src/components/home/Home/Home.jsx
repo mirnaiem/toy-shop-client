6a28de38d1e0edf7763c007e048d42e44e4797db
mirnaiem <mirnaiem8teen@gmail.com>
2023-05-18 16:30:23 +0600

    gallery section done

--- src/components/home/Home/Home.jsx
+++ src/components/home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Gallery from './Gallery/Gallery';
+
 
 
 const Home = () => {
  return (
   <div>
    <Banner></Banner>
+   <Gallery></Gallery>
+  
   </div>
  );
 };
